9e885a284e9d4457e59cf0af110e2537ea7a0b68
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-07-26 11:37:40 -0400

    added backend routing logic

---
+++ Models/transaction.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    date: "July 26",
+    name: "internet",
+    amount: 54.25,
+    from: "Verizon",
+  },
+  {
+    date: "July 10",
+    name: "coffee",
+    amount: 2,
+    from: "Dunkin' Donuts",
+  },
+  {
+    date: "June 1",
+    name: "Passport Renewal",
+    amount: 75.33,
+    from: "Passport Office",
+  },
+];
